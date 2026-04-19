04aaa0095619182fc092ff6ab780700bafd7182d
Shreyansh Saagar <shreyansh@tritv65.com>
2026-04-20 01:32:09 +0530

    CORS issue resolve for ui --Shreyansh Saagar

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",
         "http://127.0.0.1:4200",
-        "https://persona-ai-ui.vercel.app"
+        "https://persona-ai-ui-five.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
